ce6d193a6b8ccaf62b0477ab9db2c5d2dec50cc1
Brian Mego <brianmego@gmail.com>
2020-09-02 22:35:16 -0500

    Rooms update with click

--- src/App.js
+++ src/App.js
@@ -1,4 +1,5 @@
 import React, { useState } from 'react';
+import ProgressComponent from '@material-ui/core/CircularProgress';
 
 import { Jutsu } from 'react-jutsu';
 
@@ -9,12 +10,31 @@ const App = () => {
         "Spiritual": "STM/Spiritual",
         "Food": "STM/Food",
     }
+    const [showRoom, setShowRoom] = useState(false);
+    const [loading, setLoading] = useState(false);
     const [room, setRoom] = useState(null);
-    const userInfo = {'displayName': 'MyName'}
+    const [displayName, setDisplayName] = useState();
+
+    function delay(ms) {
+      return new Promise(resolve => setTimeout(resolve, ms));
+    }
 
     const joinMeeting = (roomName) => {
-        setRoom(null);
-        setRoom(<Jutsu roomName={roomName} userInfo={userInfo} />);
+        setShowRoom(false);
+        setLoading(true);
+        delay(1000).then( () => {
+            setRoom(
+                <Jutsu subject={roomName}
+                    roomName={roomName} 
+                    displayName={displayName}
+                    onMeetingEnd={() => console.log('Meeting has ended')} 
+                    containerStyles={{ width: window.innerWidth, height: window.innerHeight - 100 }}
+                />
+            );
+            setLoading(false);
+            setShowRoom(true);
+        }
+        )
     }
 
     const roomElems = []
@@ -22,18 +42,23 @@ const App = () => {
         ([key, value]) => {
             roomElems.push(
                 <StartButton key={value}
-                    roomName={value} 
-                    roomAlias={key} 
+                    roomName={value}
+                    roomAlias={key}
                     callBack={joinMeeting}/>
             )
         }
     )
+
     return (
         <div>
             <div>
                 {roomElems}
             </div>
-            {room}
+            <label>Your Name (Ignore the prompt after entering to change this): </label>
+            <input value={displayName} onChange={(e) => {setDisplayName(e.target.value) }}/>
+            <br />
+            {loading && <ProgressComponent/>}
+            {showRoom && room}
         </div>
     )
 }
