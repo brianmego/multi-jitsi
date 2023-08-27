fe477372b5a2a698d4a22b4a380395ebc8d9f23c
Brian Mego <brianmego@gmail.com>
2023-08-27 15:31:50 -0500

    Add rebuild script

---
+++ rebuild_updates.sh
@@ -0,0 +1,13 @@
+#!/bin/bash
+git fetch
+LOCAL_HASH=$(git log --format=%H -1 master)
+REMOTE_HASH=$(git log --format=%H -1 origin/master)
+echo "This commit: $LOCAL_HASH"
+echo "Remote commit: $REMOTE_HASH"
+
+if [[ "$LOCAL_HASH" != "$REMOTE_HASH" ]]; then
+    git pull
+    cd ~/multi-jitsi
+    nvm use
+    yarn build
+fi
